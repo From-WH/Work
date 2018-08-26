558d9619176a4d68788c8b915f835702b09cd0b5
wanghang <from-wh@hotmail.com>
2018-08-26 13:24:45 +0800

    AJAX笔试练习

--- AJAX-demo.js
+++ AJAX-demo.js
@@ -1,15 +1,18 @@
+// AJAX练习
+
 button.addEventListener('click',(e)=>{
   let request = new XMLHttpRequest()
   request.open('get','/xxx')
   request.send()
   request.onreadystatechange = ()=>{
-    if(request.status === 4){
-      console.log('请求完成');
+    if(request.state === 4){
+      console.log('请求完成')
       if(request.status > 200 && request.status < 300){
-        console.log('请求成功');
-        let string = request.responseText
-        let obj = window.JSON.parse(string)
-      }else if(request.status > 300){
+        console.log('请求成功')
+        //把符合JSON的字符串转换为JS对应的值
+        let string = request.JSON.responseText
+        let object = window.JSON.parse(string)
+      }else if(request.status>300){
         console.log('请求失败')
       }
     }
